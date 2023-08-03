538f615d324d6450f7255721eeb9ca4913f00a38
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-08-03 16:07:23 +0300

    workProcess

--- src/components/WorkProcess/WorkProcess.jsx
+++ src/components/WorkProcess/WorkProcess.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./WorkProcess.css";
+import WorkProcessVid from "../../assets/videos/video.mp4";
+import { ControlBar, Player, PlayToggle } from "video-react";
+import "video-react/dist/video-react.css";
 
 const WorkProcess = () => {
   return (
-    <div>WorkProcess</div>
-  )
-}
+    <div className="workprocess section__padding">
+      <div className="container">
+        <div className="workprocess__content text__center">
+          <h2 className="section__title text__cap">Our work process</h2>
+          <p className="para__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique,
+            autem perferendis? Tenetur iusto deserunt itaque nisi fugiat id
+            possimus odio.
+          </p>
 
-export default WorkProcess
+          <div className="vid__container">
+            <Player autoPlay loop src={WorkProcessVid}>
+              <ControlBar autoHide={false} disableDefaultControls={true}>
+                <PlayToggle />
+              </ControlBar>
+            </Player>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkProcess;
